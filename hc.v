8f63ee4bbc741c29350b1ba1d36ef5e774e1148d
sethkuou <itisto333@hotmail.com>
2018-11-06 17:59:30 -0700

    resolve conflixts

--- hc.v
+++ hc.v
@@ -1,1 +1,16 @@
+module hc(clk, a, b, out)
+	input [7:0] a, g;
+	output out;
+	reg AT, GT;
 
+always@*
+begin
+	if(a>g) begin
+		out = 1;
+	end
+	else if (g>a) begin
+		out = 0;
+	end
+		
+end
+endmodule
