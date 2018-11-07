44c837e626ff74245622ea1a29aaaae80b175dbe
sethkuou <itisto333@hotmail.com>
2018-11-06 21:30:32 -0700

    wrewrote hc

--- hc.v
+++ hc.v
@@ -1,16 +1,36 @@
-module hc(clk, a, b, out)
-	input [7:0] a, g;
-	output out;
-	reg AT, GT;
+`define TH 8'd10
 
-always@*
-begin
-	if(a>g) begin
-		out = 1;
+module hc(clk, rst, AT, GT, out)
+	input clk;
+	input rst;
+	input [7:0] AT, GT;
+	output AT_out, GT_out;
+	parameter s_AT_out = 1, s_GT_out = 0;
+	reg state, next_state;
+	
+	// OUTPUT COMBINATIONAL LOGIC
+	assign AT_out = (state == s_AT_out);
+	assign GT_out = (state == s_GT_out);
+	
+	// UPDATE STATE SEQUENTIAL LOGIC
+	always@(posedge clk)
+	begin
+		if(rst) state = s_AT_out;
+		else state = next_state;s
 	end
-	else if (g>a) begin
-		out = 0;
-	end
+	
+	// NEXT STATE COMBINATIONAL LOGIC
+	always@(AT or GT or state)
+	begin
+	case(state)
+		s_GT_out: if((GT < (AT-`TH)) next_state = s_AT_out;
+		default: 
+		begin
+			if(AT < (GT - `TH)) next_state = s_GT_out;
+			else if(AT > (GT - `TH)) next_state = s_AT_out;
+			else next_state = next_state;
+		end
+	endcase
 		
-end
+	end
 endmodule
