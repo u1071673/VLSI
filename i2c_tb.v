1199e1ccb7e5f8f8d9ede207e682bdf2262e8d08
JohnYoungDT <u1071673@utah.edu>
2018-11-11 11:42:30 -0700

    Auto indent

--- i2c_tb.v
+++ i2c_tb.v
@@ -1,15 +1,15 @@
 
 
-module i2c_tb ; 
+module i2c_tb;
 
   // INPUTS
-  reg rst; 
+  reg rst;
   reg clk;
-  reg [7:0] data; 
-  reg [6:0] addr; 
+  reg [7:0] data;
+  reg [6:0] addr;
 
   // OUTPUTS
-  wire sda; 
+  wire sda;
   wire scl;
 
   // INSTANTIATE THE UNIT UNDER TEST (UUT)
@@ -20,23 +20,23 @@ module i2c_tb ;
       .data(data),
       .clk(clk),
       .addr(addr)
-  ); 
+  );
 
- always #1 clk = ~clk;
+  always #1 clk = ~clk;
 
- initial begin
- // INITIALIZE INPUTS
- clk = 0;
- rst = 1;
- addr  = 7'h50 ;	  
- data  = 8'haa ;
- // WAIT 100ns FOR GLOBAL RESET TO FINISH
- #10;
- // ADD STIMULUS HERE
- rst = 0;
+  initial begin
+    // INITIALIZE INPUTS
+    clk = 0;
+    rst = 1;
+    addr  = 7'h50 ;
+    data  = 8'haa ;
+    // WAIT 100ns FOR GLOBAL RESET TO FINISH
+    #10;
+    // ADD STIMULUS HERE
+    rst = 0;
 
- #100;
- 
- end
+    #100;
+
+  end
 
 endmodule
