04f4ed8d1abc93fae89559cdfbf1b9cdec84ebf4
johny <johny@lab1-5.eng.utah.edu>
2018-11-16 22:35:51 -0700

    Implementing 2 byte reads and writes for i2c

--- i2c.v
+++ i2c.v
@@ -4,26 +4,27 @@ input wire clk,
 input wire rst,
 input wire start,
 input wire [6:0] addr, /* Set this to the address of the slave. */
-input wire [7:0] data, /* Set this to the data we want to send to the slave */
-input wire [3:0] bytes, /* Set this to the number of byte you want to read or right */
+input wire [15:0] data, /* Set this to the data we want to send to the slave */
+input wire two_bytes, /* Set this to 1 for reading or writing two data bytes. 0 means only read or write one data byte */
 input wire rw, /* 0 = write, 1 = read */
 inout wire sda,
 inout wire scl,
-output wire [7:0] read_data,
+output wire [15:0] read_data, /* This is set to the data retrieved from the slave */
 output wire ready
 );
 
-localparam [7:0] STATE_IDLE = 8'd0, STATE_START = 8'd1, STATE_ADDR = 8'd2, STATE_RW = 8'd3, STATE_SLAVE_WACK = 8'd4, STATE_WDATA = 8'd5, STATE_RDATA = 8'd6, STATE_MASTER_WACK = 8'd7, STATE_STOP = 8'd8;
-reg [7:0] state, next_state, count, next_count, latched_data;
+localparam [7:0] STATE_IDLE = 8'd0, STATE_START = 8'd1, STATE_ADDR = 8'd2, STATE_RW = 8'd3, STATE_SLAVE_WACK = 8'd4, STATE_W_LSBYTE = 8'd5, STATE_W_MSBYTE = 8'd6, STATE_R_LSB = 8'd7, STATE_R_MSB = 8'd8, STATE_MASTER_WACK = 8'd9, STATE_STOP = 8'd10;
+reg [15:0] latched_data;
+reg [7:0] state, next_state, count, next_count;
 reg [6:0] latched_addr;
-reg [3:0] latched_bytes;
+reg latched_two_bytes;
 reg scl_enable, next_scl_enable, sda_enable, next_sda_enable, latched_rw;
 reg initialized;
 // OUTPUT COMBINATIONAL LOGIC
 assign sda = sda_enable ? 1'b0 : 1'bz;
 assign scl = scl_enable && clk ? 1'b0 : 1'bz;
 assign ready = (state == STATE_IDLE) && !(rst);
-assign read_data = latched_rw ? latched_data : 8'bxxxxxxxx;
+assign read_data = latched_rw ? latched_data : 16'bxxxxxxxxxxxxxxxx;
 
 // UPDATE STATE SEQUENTIAL LOGIC
 always@(posedge clk)
@@ -34,17 +35,19 @@ begin
   end
   else if (initialized)
   begin
+  
     if(state == STATE_IDLE)
     begin
         latched_addr <= addr;
 	latched_data <= data;
 	latched_rw <= rw;
-	latched_bytes <= bytes;
+	latched_two_bytes <= two_bytes;
     end
     else if(state == STATE_RDATA)
     begin
       latched_data[count] = (sda === 1'bz);
     end
+
     state <= next_state;
     count <= next_count;
     sda_enable <= next_sda_enable;
@@ -59,13 +62,13 @@ begin
     latched_addr <= 7'd0;
     latched_data <= 8'd0;
     latched_rw <= 1'd0;
-    latched_bytes <= 1'd0;
+    latched_two_bytes <= 1'd0;
     initialized <= 1'd1;
   end
 end
 
 // NEXT STATE COMBINATIONAL LOGIC (Only set 'next_' wires)
-always@(sda or scl or state or start or count or latched_rw or latched_data or latched_addr or latched_bytes)
+always@(sda or scl or state or start or count or latched_rw or latched_data or latched_addr or latched_two_bytes)
 begin
     next_sda_enable = 1'b0;
     next_scl_enable = 1'b0;
@@ -120,16 +123,34 @@ begin
       end 
       else // ACK
       begin
-	next_count = 8'd7;
       	next_scl_enable = 1'b1;
 	if(latched_rw) // reading (1 = reading)
         begin
-          next_state = STATE_RDATA;
+	  if(latched_two_bytes)
+          begin
+            next_state = STATE_R_MSBYTE;
+	    next_count = 8'd15;
+	  end
+	  else
+   	  begin
+            next_state = STATE_R_LSBYTE;
+	    next_count = 8'd7;
+	  end
         end
         else // writing (0 = writing)
         begin
-          next_state = STATE_WDATA;
-          next_sda_enable = ~latched_data[next_count];
+	  if(latched_two_bytes)
+          begin
+            next_state = STATE_W_MSBYTE;
+	    next_count = 8'd15;
+            next_sda_enable = ~latched_data[next_count];
+	  end
+	  else
+   	  begin
+            next_state = STATE_W_LSBYTE;
+	    next_count = 8'd7;
+	    next_sda_enable = ~latched_data[next_count];
+	  end
         end
       end
     end
@@ -140,7 +161,21 @@ begin
 	next_scl_enable = 1'b1;
     end
 
-    STATE_WDATA:
+    STATE_W_MSBYTE:
+    begin
+      if(count == 8)
+      begin 
+	next_state = STATE_SLAVE_WACK;
+      end
+      else 
+      begin
+	next_count = count - 8'd1;
+        next_sda_enable = ~latched_data[next_count];
+      end
+      next_scl_enable = 1'b1;
+    end
+
+    STATE_W_LSBYTE:
     begin
       if(count == 0)
       begin 
@@ -154,7 +189,21 @@ begin
       next_scl_enable = 1'b1;
     end
 
-    STATE_RDATA:
+    STATE_R_MSB:
+    begin
+      if(count == 8)
+      begin 
+	next_state = STATE_MASTER_WACK;
+	next_sda_enable = 1'b1;
+      end
+      else 
+      begin
+	next_count = count - 8'd1;
+      end
+      next_scl_enable = 1'b1;
+    end
+
+    STATE_R_LSB:
     begin
       if(count == 0)
       begin 
