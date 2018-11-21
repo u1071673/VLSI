a9c82c7fe48510d34ba5864f8e0012fd48a7bb7e
jasonj <jasonj@lab1-13.eng.utah.edu>
2018-11-20 22:39:52 -0700

    Formated uart_clock_divider

--- uart_clock_divider.v
+++ uart_clock_divider.v
@@ -5,24 +5,24 @@ module uart_clock_divider(
   input clk_in,
   input rst,
   output clk_out
-  )
+  );
 
-reg [WIDTH-1:0] r_reg;
-wire [WIDTH-1:0] r_nxt;
+reg [`WIDTH-1:0] r_reg;
+wire [`WIDTH-1:0] r_nxt;
 reg clk_track;
 
 assign r_nxt = r_reg + 1;           
 assign clk_out = clk_track;
 
-always @(posedge clk_in or posedge reset)
+always @(posedge clk_in or posedge rst)
 begin
-  if (reset)
+  if (rst)
   begin
     r_reg <= 0;
     clk_track <= 1'b0;
   end
 
-  else if (r_nxt == N)
+  else if (r_nxt == `N)
   begin
     r_reg <= 0;
     clk_track <= ~clk_track;
--- uart_clock_divider_tb.v
+++ uart_clock_divider_tb.v
@@ -1,17 +1,22 @@
 
 module uart_clock_divider_tb;
-  reg clk_in,reset;
+  reg clk_in, rst;
   wire clk_out;
 
-  uart_clock_divider uut(clk_in,reset,clk_out);
+  uart_clock_divider uut(
+  .clk_in(clk_in),
+  .rst(rst), 
+  .clk_out(clk_out)
+  );
 
   always #5 clk_in=~clk_in; 
+
   initial
-  clk_in= 1'b0; 
   begin
-   #5 reset=1'b1;
-   #10 reset=1'b0;
-   #500 $finish;
+   clk_in = 1'b0; 
+   #5; rst = 1'b1;
+   #10; rst = 1'b0;
+   #500;
   end
 
 endmodule
