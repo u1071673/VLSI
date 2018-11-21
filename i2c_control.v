7e0b426e8043fb21c709e0465471cd5af4312a5b
JohnYoungDT <u1071673@utah.edu>
2018-11-20 20:18:48 -0700

    added output conversions for both ls and ts

--- i2c_control.v
+++ i2c_control.v
@@ -10,26 +10,38 @@
 module i2c_control (
 	input wire clk,
 	input wire rst,
-	output wire [7:0] solar_celcius,
-	output wire [7:0] greenhouse_celcius,
-	output wire [7:0] ambient_celcius,
-	output wire [7:0] geothermal_celcius,
-	output wire [7:0] n_lux,
-	output wire [7:0] e_lux,
-	output wire [7:0] s_lux,
-	output wire [7:0] w_lux
+	output wire [8:0] solar_celcius,
+	output wire [8:0] greenhouse_celcius,
+	output wire [8:0] ambient_celcius,
+	output wire [8:0] geothermal_celcius,
+	output wire [15:0] n_lux,
+	output wire [15:0] e_lux,
+	output wire [15:0] s_lux,
+	output wire [15:0] w_lux
 	);
 
 localparam [7:0] STATE_IDLE = 8'd0, STATE_SOLAR = 8'd1, STATE_GREENHOUSE = 8'd2, STATE_AMBIENT = 8'd3, STATE_GEOTHERMAL = 8'd4, STATE_NORTH = 8'd5, STATE_EAST = 8'd6, STATE_SOUTH = 8'd7, STATE_WEST = 8'd7; 
 
-wire solar_read_data;
-wire greenhouse_read_data;
-wire ambient_read_data;
-wire geothermal_read_data;
-wire north_read_data;
-wire east_read_data;
-wire south_read_data;
-wire west_read_data;
+wire [15:0] solar_read_data;
+wire [15:0] greenhouse_read_data;
+wire [15:0] ambient_read_data;
+wire [15:0] geothermal_read_data;
+wire [15:0] north_read_data;
+wire [15:0] east_read_data;
+wire [15:0] south_read_data;
+wire [15:0] west_read_data;
+wire [11:0] n_fractional;
+wire [11:0] e_fractional;
+wire [11:0] s_fractional;
+wire [11:0] w_fractional;
+wire [3:0] n_exponent;
+wire [3:0] e_exponent;
+wire [3:0] s_exponent;
+wire [3:0] w_exponent;
+wire [3:0] n_lsb_size;
+wire [3:0] e_lsb_size;
+wire [3:0] s_lsb_size;
+wire [3:0] w_lsb_size;
 wire solar_ready;
 wire greenhouse_ready;
 wire ambient_ready;
@@ -42,8 +54,8 @@ wire ready;
 wire sda;
 wire scl;
 
+reg [15:0] next_write_data;
 reg [7:0] next_state;
-reg [7:0] next_write_data;
 reg [6:0] next_slave_addr;
 reg next_solar_start;
 reg next_greenhouse_start;
@@ -56,16 +68,16 @@ reg next_west_start;
 reg next_rw;
 reg next_two_bytes;
 
-reg [7:0] latched_solar_celcius; 
-reg [7:0] latched_greenhouse_celcius; 
-reg [7:0] latched_ambient_celcius; 
-reg [7:0] latched_geothermal_celcius; 
-reg [7:0] latched_n_lux; 
-reg [7:0] latched_e_lux; 
-reg [7:0] latched_s_lux; 
-reg [7:0] latched_w_lux; 
+reg [15:0] write_data;
+reg [8:0] latched_solar_celcius; 
+reg [8:0] latched_greenhouse_celcius; 
+reg [8:0] latched_ambient_celcius; 
+reg [8:0] latched_geothermal_celcius; 
+reg [15:0] latched_n_lux; 
+reg [15:0] latched_e_lux; 
+reg [15:0] latched_s_lux; 
+reg [15:0] latched_w_lux;
 reg [7:0] state;
-reg [7:0] write_data;
 reg [6:0] slave_addr;
 reg solar_start;
 reg greenhouse_start;
@@ -80,6 +92,22 @@ reg rw;
 reg initialized;
 
 assign ready = solar_ready && greenhouse_ready && ambient_ready && geothermal_ready && north_ready && east_ready && south_ready && west_ready;
+assign n_exponent = north_read_data[15:12];
+assign e_exponent = east_read_data[15:12];
+assign s_exponent = south_read_data[15:12];
+assign w_exponent = west_read_data[15:12];
+assign n_fractional = north_read_data[11:0];
+assign e_fractional = east_read_data[11:0];
+assign s_fractional = south_read_data[11:0];
+assign w_fractional = west_read_data[11:0];
+assign n_lsb_size = (16'd1 << {12'd0, n_exponent}) / 16'd100;
+assign e_lsb_size = (16'd1 << {12'd0, e_exponent}) / 16'd100;
+assign s_lsb_size = (16'd1 << {12'd0, s_exponent}) / 16'd100;
+assign w_lsb_size = (16'd1 << {12'd0, w_exponent}) / 16'd100;
+assign calulated_n_lux = n_lsb_size * n_fractional;
+assign calulated_e_lux = e_lsb_size * e_fractional;
+assign calulated_s_lux = s_lsb_size * s_fractional;
+assign calulated_w_lux = w_lsb_size * w_fractional;
 
 i2c solar_ts (
 	.data(write_data), /* Set this to the write_data we want to send to the slave. If we are reading this should be 16'd0 */
@@ -205,35 +233,35 @@ begin
 		case(state)
 		STATE_SOLAR:
 		begin
-			if(ready) latched_solar_celcius <= solar_read_data;
+			if(ready) latched_solar_celcius <= solar_read_data[15:7];
 		end
 		STATE_GREENHOUSE:
 		begin
-			if(ready) latched_greenhouse_celcius <= greenhouse_read_data;
+			if(ready) latched_greenhouse_celcius <= greenhouse_read_data[15:7];
 		end
 		STATE_AMBIENT:
 		begin
-			if(ready) latched_ambient_celcius <= ambient_read_data;
+			if(ready) latched_ambient_celcius <= ambient_read_data[15:7];
 		end
 		STATE_GEOTHERMAL:
 		begin
-			if(ready) latched_geothermal_celcius <= geothermal_read_data;
+			if(ready) latched_geothermal_celcius <= geothermal_read_data[15:7];
 		end
 		STATE_NORTH:
 		begin
-			if(ready) latched_n_lux <= north_read_data;
+			if(ready) latched_n_lux <= calulated_n_lux;
 		end
 		STATE_EAST:
 		begin
-			if(ready) latched_e_lux <= east_read_data;
+			if(ready) latched_e_lux <= calulated_e_lux;
 		end
 		STATE_SOUTH:
 		begin
-			if(ready) latched_s_lux <= south_read_data;
+			if(ready) latched_s_lux <= calulated_s_lux;
 		end
 		STATE_WEST:
 		begin
-			if(ready) latched_w_lux <= west_read_data;
+			if(ready) latched_w_lux <= calulated_w_lux;
 		end
 		endcase
 	end
---
+++ i2c_control_tb.sv
@@ -0,0 +1,172 @@
+`timescale 1ns / 1ps
+
+module i2c_control_tb;
+
+  localparam [3:0] TEST1 = 4'd1, TEST2 = 4'd2, TEST3 = 4'd3, TEST4 = 4'd4, TEST5 = 4'd5, TEST6 = 4'd6, TEST7 = 4'd7, TEST8 = 4'd8, TEST9 = 4'd9, TEST10 = 4'd10, TEST11 = 4'd11;
+  reg [3:0] test;
+  reg [15:0] transmit_data;
+  wire pull_sda_low;
+
+  // INPUTS
+  reg rw;
+  reg rst;
+  reg clk;
+  reg start;
+  reg two_bytes;
+  reg [6:0] addr;
+  reg [15:0] data;
+
+  // OUTPUTS
+  wire sda;
+  wire scl;
+  wire ready;
+  wire [15:0] read_data;
+
+  // INSTANTIATE THE UNIT UNDER TEST (UUT)
+  i2c uut(
+    .sda(sda),
+    .ready(ready),
+    .rst(rst),
+    .start(start),
+    .scl(scl),
+    .addr(addr),
+    .data(data),
+    .two_bytes(two_bytes),
+    .read_data(read_data),
+    .rw(rw),
+    .clk(clk)
+    );
+
+  always #2.5 clk = ~clk;
+  assign pull_sda_low =   (
+                         ((test == TEST5 || test == TEST6 || test == TEST7|| test == TEST8) && uut.state == 4 && bytes_written < 1) || // Slave acknowedge states
+                          ((uut.state == 7 || uut.state == 8) && ~transmit_data[uut.count]) ||
+                          test == TEST9 || test == TEST11
+                          );
+  
+  assign sda = pull_sda_low ? 1'b0 : 1'bz;
+  assign scl = test == TEST10 || test == TEST11 ? 1'b0 : 1'bz;
+
+  always@(posedge uut.state)
+  begin
+    if(uut.state == 0 || uut.state == 1) bytes_written <= 0;
+    else if(uut.state == 5 || uut.state == 7) bytes_written <= bytes_written + 1;
+    else;
+  end
+
+
+
+  initial begin
+    // INITIALIZE INPUTS
+    addr  = 7'h50 ;
+    data = 0;
+    clk = 0;
+    rst = 0;
+    rw = 0;
+    test = 0;
+    start = 0;
+    two_bytes = 0;
+    transmit_data = 0;
+    bytes_written = 0;
+
+    // WAIT 10ns FOR GLOBAL RESET TO FINISH
+    #10;
+    // ADD STIMULUS HERE
+    test = TEST1; // Test for one byte write without slave acknowledge
+    rw = 0;
+    two_bytes = 0;
+    data  = 16'haa55;
+    transmit_data = 16'h0000; 
+    start = 1; #5;
+    start = 0; #5;
+
+    #75;
+
+    test = TEST2; // Test for two byte write without slave acknowledge
+    rw = 0;
+    two_bytes = 1;
+    data  = 16'haa55;
+    transmit_data = 16'h0000; 
+    start = 1; #5;
+    start = 0; #5;
+
+    #75;
+
+    test = TEST3; // Test for one byte read without slave acknowledge
+    rw = 1;
+    two_bytes = 0;
+    transmit_data = 16'ha0a0;
+    data  = 16'h0000;
+    start = 1; #5;
+    start = 0; #5;
+
+    #75;
+
+    test = TEST4; // Test for two byte read without slave acknowledge
+    rw = 1;
+    two_bytes = 1;
+    data  = 16'h0000;
+    transmit_data = 16'ha0a0;
+    start = 1; #5;
+    start = 0; #5;
+
+    #75;
+
+    test = TEST5; // Test for one byte write with slave acknowledge
+    rw = 0;
+    two_bytes = 0;
+    data  = 16'haa55;
+    transmit_data = 16'h0000; 
+    start = 1; #5;
+    start = 0; #5;
+
+    #100;
+
+    test = TEST6; // Test for two byte write with slave acknowledge
+    rw = 0;
+    two_bytes = 1;
+    data  = 16'haa55;
+    transmit_data = 16'h0000;
+    start = 1; #5;
+    start = 0; #5;
+
+    #200;
+
+    test = TEST7; // Test for one byte read with slave acknowledge
+    rw = 1;
+    two_bytes = 0;
+    data  = 16'h0000;
+    transmit_data = 16'ha7b8; 
+    start = 1; #5;
+    start = 0; #5;
+
+    #100;
+
+    test = TEST8; // Test for two byte read with slave acknowledge
+    rw = 1;
+    two_bytes = 1;
+    data  = 16'h0000;
+    transmit_data = 16'ha7b8; 
+    start = 1; #5;
+    start = 0; #5;
+
+    #200;
+
+    test = TEST9; // Testing ready line to ensure we don't say i2c is ready when another device is using it.
+    #25;
+    test = 0;
+    #25;
+
+    test = TEST10; // Testing ready line to ensure we don't say i2c is ready when another device is using it.
+    #25;
+    test = 0;
+    #25;
+    
+    test = TEST11;
+    #25;
+    test = 0;
+    #25;
+
+  end
+
+endmodule
