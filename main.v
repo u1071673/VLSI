6dda0187cd8789ff1a65b63da5666ee0637af69b
JohnYoungDT <u1071673@utah.edu>
2018-11-23 21:27:37 -0700

    Added sev seg to main

--- main.v
+++ main.v
@@ -13,7 +13,51 @@ module main(
 	output wire move_north,
 	output wire move_east,
 	output wire move_south,
-	output wire move_west
+	output wire move_west,
+	output wire solar_anode0_en,
+	output wire solar_anode1_en,
+	output wire solar_anode2_en,
+	output wire solar_anode3_en,
+	output wire greenhouse_anode0_en,
+	output wire greenhouse_anode1_en,
+	output wire greenhouse_anode2_en,
+	output wire greenhouse_anode3_en,
+	output wire ambient_anode0_en,
+	output wire ambient_anode1_en,
+	output wire ambient_anode2_en,
+	output wire ambient_anode3_en,
+	output wire geothermal_anode0_en,
+	output wire geothermal_anode1_en,
+	output wire geothermal_anode2_en,
+	output wire geothermal_anode3_en,
+	output wire solar_a_out,
+	output wire solar_b_out,
+	output wire solar_c_out,
+	output wire solar_d_out,
+	output wire solar_e_out,
+	output wire solar_f_out,
+	output wire solar_g_out,
+	output wire greehouse_a_out,
+	output wire greehouse_b_out,
+	output wire greehouse_c_out,
+	output wire greehouse_d_out,
+	output wire greehouse_e_out,
+	output wire greehouse_f_out,
+	output wire greehouse_g_out,
+	output wire ambient_a_out,
+	output wire ambient_b_out,
+	output wire ambient_c_out,
+	output wire ambient_d_out,
+	output wire ambient_e_out,
+	output wire ambient_f_out,
+	output wire ambient_g_out,
+	output wire geothermal_a_out,
+	output wire geothermal_b_out,
+	output wire geothermal_c_out,
+	output wire geothermal_d_out,
+	output wire geothermal_e_out,
+	output wire geothermal_f_out,
+	output wire geothermal_g_out
 	);
 // To Do: Add vdd and gnd wires to chip
 wire [15:0] n_lux;
@@ -133,7 +177,7 @@ temp_control temp_control_geothermal(
 	);
 
 
-// GEOTHERMAL MODULES
+// SOLARHEATER MODULES
 hc hc_solarheater(
 	.clk(clk),
 	.rst(rst),
@@ -151,6 +195,79 @@ temp_control temp_control_solarheater(
 	.temp_g_greenhouse_temp(solar_g_greenhouse_temp),
 	.out(solarheater_on)
 	);
+
+// SOLAR CELCIUS SEVEN SEG
+seven_segment_controller solar_sev_seg(
+	.clk(clk),
+	.rst(rst),
+	.binary(solar_celcius), 
+	.anode0_en(solar_anode0_en),
+	.anode1_en(solar_anode1_en),
+	.anode2_en(solar_anode2_en),
+	.anode3_en(solar_anode3_en),
+	.a_out(solar_a_out),
+	.b_out(solar_b_out),
+	.c_out(solar_c_out),
+	.d_out(solar_d_out),
+	.e_out(solar_e_out),
+	.f_out(solar_f_out),
+	.g_out(solar_g_out)
+	);
+
+// GREENHOUSE_CELCIUS SEVEN SEG
+seven_segment_controller greenhouse_sev_seg(
+	.clk(clk),
+	.rst(rst),
+	.binary(greenhouse_celcius),
+	.anode0_en(greenhouse_anode0_en),
+	.anode1_en(greenhouse_anode1_en),
+	.anode2_en(greenhouse_anode2_en),
+	.anode3_en(greenhouse_anode3_en),
+	.a_out(greehouse_a_out),
+	.b_out(greehouse_b_out),
+	.c_out(greehouse_c_out),
+	.d_out(greehouse_d_out),
+	.e_out(greehouse_e_out),
+	.f_out(greehouse_f_out),
+	.g_out(greehouse_g_out)
+	);
+
+// AMBIENT_CELCIUS SEVEN SEG
+seven_segment_controller ambient_sev_seg(
+	.clk(clk),
+	.rst(rst),
+	.binary(ambient_celcius),
+	.anode0_en(ambient_anode0_en),
+	.anode1_en(ambient_anode1_en),
+	.anode2_en(ambient_anode2_en),
+	.anode3_en(ambient_anode3_en),
+	.a_out(ambient_a_out),
+	.b_out(ambient_b_out),
+	.c_out(ambient_c_out),
+	.d_out(ambient_d_out),
+	.e_out(ambient_e_out),
+	.f_out(ambient_f_out),
+	.g_out(ambient_g_out)
+	);
+
+// GEOTHERMAL_CELCIUS SEVEN SEG
+seven_segment_controller geothermal_sev_seg(
+	.clk(clk),
+	.rst(rst),
+	.binary(geothermal_celcius),
+	.anode0_en(geothermal_anode0_en),
+	.anode1_en(geothermal_anode1_en),
+	.anode2_en(geothermal_anode2_en),
+	.anode3_en(geothermal_anode3_en),
+	.a_out(geothermal_a_out),
+	.b_out(geothermal_b_out),
+	.c_out(geothermal_c_out),
+	.d_out(geothermal_d_out),
+	.e_out(geothermal_e_out),
+	.f_out(geothermal_f_out),
+	.g_out(geothermal_g_out)
+	);
+
 
 
 endmodule // main
